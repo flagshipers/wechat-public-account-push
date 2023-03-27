bd5620c02961d30919c6945ddf208a037b66a118
flagshipers <128790313+flagshipers@users.noreply.github.com>
2023-03-27 15:05:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '111',
+  CALLBACK_TEMPLATE_ID: '-mieV3AMKGn0Z7LOOUVeW8zoNxogG80__4pgVlyADcA',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'y_a0795',
+      id: 'o29_X6FRdS6iJ25UFX5DbydaNycU',
     }
   ],
 
