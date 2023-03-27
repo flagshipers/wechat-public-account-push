749a514b26695a8d6c3929b6a855692ea7b67e70
flagshipers <128790313+flagshipers@users.noreply.github.com>
2023-03-27 14:58:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -57,7 +57,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o29_X6FRdS6iJ25UFX5DbydaNycU',
+      id: 'y_a0795',
     }
   ],
 
